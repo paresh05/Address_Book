9865091508d131b4a0f87df388dc7fb4a36b3c69
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-08-30 23:35:08 +0530

    Added a method to delete contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,73 +1,122 @@
 package com.bridgelabzd12;  
 import java.util.*;  
 public class AddressBook {
+	public static ArrayList<ContactInfo> contactInfo;
+	
+	public AddressBook() {
+		contactInfo = new ArrayList<ContactInfo>();
+	}
+    private static ContactInfo getPerson(ArrayList<ContactInfo> contactInfo){
+        for(int i=0;i<contactInfo.size();){
+                return contactInfo.get(i);
+        }
+        return null;
+    }
+	public void addContact() {
+		Scanner sc = new Scanner(System.in);  
+		System.out.println("Enter First Name");
+		String firstName=sc.nextLine();
+		System.out.println("Enter Last Name");
+		String lastName=sc.nextLine();
+		System.out.println("Enter Address");
+		String address=sc.nextLine();
+		System.out.println("Enter city");
+		String city=sc.nextLine();
+		System.out.println("Enter state");
+		String state=sc.nextLine();
+		System.out.println("Enter zip code");
+		String zipCode=sc.nextLine();
+		System.out.println("Enter Phone Number");
+		String phoneNumber=sc.nextLine();
+		System.out.println("Enter Email");
+		String email=sc.nextLine();
+		
+		ContactInfo contact = new ContactInfo(firstName, lastName, address, city,state,  zipCode, phoneNumber,email);
+		contactInfo.add(contact);
+		contact.print();
+		//edit(firstName,lastName,address,city,state,zipCode,phoneNumber,email);//Function call to edit function
+	}
+	public void deleteContact() {
+		for(int i=0;i<contactInfo.size();i++) {
+			Scanner sc = new Scanner(System.in);  
+			System.out.println("Enter the name");
+			String input=sc.nextLine();
+			ContactInfo obj = (ContactInfo)contactInfo.get(i);
+			if(input.equals(obj.getFirstName())){
+				contactInfo.remove(i);
+				System.out.println("Contact Removed");
+			}
+		}
+	}
 	public static void main(String[] args) {
 			System.out.println("Welcome to Address Book");
-			Scanner sc = new Scanner(System.in);  
-			System.out.println("Enter First Name");
-			String firstName=sc.nextLine();
-			System.out.println("Enter Last Name");
-			String lastName=sc.nextLine();
-			System.out.println("Enter Address");
-			String address=sc.nextLine();
-			System.out.println("Enter city");
-			String city=sc.nextLine();
-			System.out.println("Enter state");
-			String state=sc.nextLine();
-			System.out.println("Enter zip code");
-			String zipCode=sc.nextLine();
-			System.out.println("Enter Phone Number");
-			String phoneNumber=sc.nextLine();
-			System.out.println("Enter Email");
-			String email=sc.nextLine();
-			// Object Creation
-			ContactInfo obj = new ContactInfo(firstName, lastName, address, city,state,  zipCode, phoneNumber,email);
-			obj.print();
-			edit(firstName,lastName,address,city,state,zipCode,phoneNumber,email);//Function call to edit function
+			AddressBook ab = new AddressBook();
+			int choice,exit=0;
+			while(exit==0) {
+				System.out.println("Enter 1 to add contact, 2 to edit contact, 3 to delete contact, and 4 to exit");
+				Scanner sc = new Scanner(System.in);  
+				System.out.println("Enter your choice");
+				choice=sc.nextInt();
+				switch(choice) {
+				case 1:
+					ab.addContact();
+					break;
+				case 2:
+					ContactInfo personInfo=getPerson(contactInfo);
+					ab.edit(personInfo);
+					break;
+				case 3:
+					ab.deleteContact();
+					break;
+				case 4:
+					exit=1;
+					break;
+				}
+			}			
 	}
 	
 	/* Function Edit
 	 * This function is used to edit the information entered by the user*/
-	public static void edit(String firstName, String lastName, String address, String city, String state, String zipCode, String phoneNumber, String email)
+	public void edit(ContactInfo contact)
 	{
-		ContactInfo obj = new ContactInfo(firstName, lastName, address, city,state,  zipCode, phoneNumber,email);
+		contact.getFirstName();
 		System.out.println("Which information do you want to edit");
 		Scanner sc = new Scanner(System.in);  
 		String editInfo=sc.nextLine().toLowerCase();
 		System.out.println(editInfo);
 		if(editInfo.equals("first name")) {
 			System.out.println("Re-enter the First Name");
-			obj.setFirstName(sc.nextLine());
+			contact.setFirstName(sc.nextLine());
 		}
 		else if(editInfo.equals("last name")) {
 			System.out.println("Re-enter the Last Name");
-			obj.setLastName(sc.nextLine());
+			contact.setLastName(sc.nextLine());
 		}
 		else if(editInfo.equals("address")) {
 			System.out.println("Re-enter the address");
-			obj.setAddress(sc.nextLine());
+			contact.setAddress(sc.nextLine());
 		}
 		else if(editInfo.equals("city")) {
 			System.out.println("Re-enter the city");
-			obj.setCity(sc.nextLine());
+			contact.setCity(sc.nextLine());
 		}
 		else if(editInfo.equals("state")) {
 			System.out.println("Re-enter the state");
-			obj.setState(sc.nextLine());
+			contact.setState(sc.nextLine());
 		}
 		else if(editInfo.equals("zip code")) {
 			System.out.println("Re-enter the Zip Code");
-			obj.setZipCode(sc.nextLine());
+			contact.setZipCode(sc.nextLine());
 		}
 		else if(editInfo.equals("phone number")) {
 			System.out.println("Re-enter the Phone number");
-			obj.setPhoneNumber(sc.nextLine());
+			contact.setPhoneNumber(sc.nextLine());
 		}
 		else if(editInfo.equals("email")) {
 			System.out.println("Re-enter the Email");
-			obj.setEmail(sc.nextLine());
+			contact.setEmail(sc.nextLine());
 		}
 		// Printing the edited function
-		obj.print();
+		contact.print();
 	}
 }
